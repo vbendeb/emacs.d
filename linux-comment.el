6de66d9db6afed3922747783165861c2f0e5b4fe
Vadim Bendebury <vbendeb@chromium.org>
2016-12-31 11:28:04 -0800

    improve linux-comment implementation
    Change-Id: Icbae5e330d115a0fba649f244ea94ddbb397a68e
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- linux-comment.el
+++ linux-comment.el
@@ -1,8 +1,8 @@
 (require 'cl)
 (defun get-prefix (string)
   "replace with spaces all printable characers but the last one
-in the passed in string, then convert series of spaces into tabs
-as appropriate and return the generated string"
+   in the passed in string, then convert series of spaces into
+   tabs as appropriate and return the generated string"
 
   (with-temp-buffer
     (insert string)
@@ -15,72 +15,62 @@ as appropriate and return the generated string"
     (tabify (point-min) (point-max))
     (buffer-string)))
 
-(defun* fix-linux-comment()
-  "reformat a comment block as created by emacs c-fill-paragraph
-into a linux kernel style compliant block."
+(defun single-line-comment(open-point close-point)
+  (eq (line-number-at-pos open-point) (line-number-at-pos close-point)))
+
+(defun* c-comment-handler()
+  "reformat a comment block."
   (interactive)
   (let ((current-position (point))
-	(openc "^.*/\\*")
+	(openc "/\\*")
 	(closec "\\*/")
 	open-point
 	close-point
-	eol-point
-	x-point
-	prefix)
-    (save-excursion)
-
+	next-open-point
+	line-start)
     ; find where the comment starts and ends
-    (setq open-point (re-search-backward openc 0 t)
-	  close-point (re-search-forward closec (buffer-size) t))
+    (save-excursion (setq open-point (re-search-backward openc 0 t)))
+    (save-excursion (setq close-point (re-search-forward closec (buffer-size) t)))
+    (save-excursion (setq next-open-point (re-search-forward openc (buffer-size) t)))
 
     ; verify that the comment indeed exists and the point is within the
     ; comment block.
     (if (or (eq nil open-point)
 	    (eq nil close-point)
-	    (< close-point current-position))
-	(progn (message "not in a comment block %d %d %d"
-			open-point
-			current-position
-			close-point)
-	       (return-from fix-linux-comment nil)))
+	    (< next-open-point close-point))
+	(progn (message "not in a comment block")
+	       (return-from c-comment-handler nil)))
+    (delete-trailing-whitespace open-point close-point)
+    (c-fill-paragraph)
+    (save-excursion (setq open-point (re-search-backward openc 0 t)))
+    (save-excursion (setq close-point
+			  (re-search-forward closec (buffer-size) t)))
+    (if (single-line-comment open-point close-point)
+	(return-from c-comment-handler nil))
+    (save-excursion
+      ; let's handle the start of the comment.
+      (re-search-backward openc 0 t)
+      (forward-char 2)
+      (setq new-text (buffer-substring (line-beginning-position) (point)))
+      (setq new-text (replace-regexp-in-string "/\\*" " *" new-text))
+      (if (not (eolp))
+	  (insert (concat "\n" new-text)))
 
-    ; Inset a newline after the opening pattern if it is not on a separate
-    ; line.
-    (goto-char open-point)
-    (setq eol-point (progn (end-of-line) (point)))
-    (goto-char open-point)
-    (setq x-point (re-search-forward openc close-point))
-    (if (not (equal x-point eol-point))
-	(progn (newline)
-	       (previous-line)
-	       (end-of-line)))
+      ; Now let's handle the end of the comment.
+      (re-search-forward closec (buffer-size) t)
+      (if (re-search-backward "^[[:blank:]]*\\*/" (line-beginning-position) t)
+	  (replace-match (concat new-text "/"))
+	(forward-char -3)
+	(delete-char 3)
+	(insert (concat "\n" new-text "/\n")))
 
-    ; generate a prefix pattern from the opening line of the comment
-    (setq prefix (get-prefix (buffer-substring open-point (point))))
-
-    (setq open-point (progn (next-line 2) (point)))
-
-    ; get to the end of comment
-    (re-search-forward closec (buffer-size))
-
-    ; replace the space before closing sequence with a newline
-    (forward-char -3)(delete-char 1)(newline)
-
-    ; insert prefix before the closing sequence (removing the last two
-    ; charactes of the prefix) - which completes the last line of the
-    ; multiline C comment
-    (insert prefix)
-    (delete-char -2)
-
-    ; now replace all lines' prefixes with the comment prefix.
-    (while (> (point) open-point)
-      (beginning-of-line)
-      (setq eol-point (point))
-      (previous-line)
-      (re-search-forward "^[[:blank:]]*" eol-point)
-      (replace-match prefix))))
-
-(defun* linux-comment()
-  (interactive)
-  (c-fill-paragraph)
-  (linux-fill-paragraph))
+      ; and now let's handle the rest of the comment block
+      (goto-char open-point)
+      (forward-line 2)
+      (while (< (point) close-point)
+	(if (not (re-search-forward "^[[:blank:]]*\\*" (line-end-position) t))
+	    (if (re-search-forward "^[[:blank:]]*$" (line-end-position) t)
+		(replace-match new-text)
+	      (re-search-forward "^[[:blank:]]*" (line-end-position) t)
+	      (replace-match (concat new-text " "))))
+	(forward-line 1)))))
